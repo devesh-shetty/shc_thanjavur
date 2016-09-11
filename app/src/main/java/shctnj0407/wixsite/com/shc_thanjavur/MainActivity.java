9a7a8448faf8ca1ad4fdafb56212e8bf6d3ce82c
Devesh Shetty <devesh.shetty007@gmail.com>
2016-09-12 00:23:57 +0530

    Successfully loaded pdf from assets to pdfViewer

--- app/src/main/java/shctnj0407/wixsite/com/shc_thanjavur/MainActivity.java
+++ app/src/main/java/shctnj0407/wixsite/com/shc_thanjavur/MainActivity.java
@@ -2,12 +2,61 @@ package shctnj0407.wixsite.com.shc_thanjavur;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.shockwave.pdfium.PdfDocument;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements OnPageChangeListener, OnLoadCompleteListener {
+
+
+    private static final String TAG =   MainActivity.class.getName() ;
+    PDFView mPdfView;
+
+    Integer pageNumber = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPdfView = (PDFView) findViewById(R.id.pdfView);
+    }
+
+    public void viewHistory(View view){
+        Log.d(TAG, "button clicked");
+        mPdfView.fromAsset("church_history.pdf")
+                .defaultPage(0)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .onLoad(this)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .load();
+    }
+
+    @Override
+    public void loadComplete(int nbPages) {
+        PdfDocument.Meta meta = mPdfView.getDocumentMeta();
+        Log.e(TAG, "title = " + meta.getTitle());
+        Log.e(TAG, "author = " + meta.getAuthor());
+        Log.e(TAG, "subject = " + meta.getSubject());
+        Log.e(TAG, "keywords = " + meta.getKeywords());
+        Log.e(TAG, "creator = " + meta.getCreator());
+        Log.e(TAG, "producer = " + meta.getProducer());
+        Log.e(TAG, "creationDate = " + meta.getCreationDate());
+        Log.e(TAG, "modDate = " + meta.getModDate());
+    }
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+        pageNumber = page;
+        setTitle(String.format("%s %s / %s", "church", page + 1, pageCount));
     }
 }
---
+++ app/src/main/java/shctnj0407/wixsite/com/shc_thanjavur/SplashActivity.java
@@ -0,0 +1,17 @@
+package shctnj0407.wixsite.com.shc_thanjavur;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
