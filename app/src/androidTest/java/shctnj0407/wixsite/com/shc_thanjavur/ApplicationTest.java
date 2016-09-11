08a1efeda852801180be0c84c3e31bde773bc503
Devesh Shetty <devesh.shetty007@gmail.com>
2016-09-11 23:12:55 +0530

    Inital application

---
+++ app/src/androidTest/java/shctnj0407/wixsite/com/shc_thanjavur/ApplicationTest.java
@@ -0,0 +1,13 @@
+package shctnj0407.wixsite.com.shc_thanjavur;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/shctnj0407/wixsite/com/shc_thanjavur/MainActivity.java
@@ -0,0 +1,13 @@
+package shctnj0407.wixsite.com.shc_thanjavur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
